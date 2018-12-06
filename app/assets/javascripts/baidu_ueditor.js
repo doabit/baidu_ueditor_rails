5f62b4c37364f01c5f41836d9f80ab7405afb7f3
doabit <doinsist@gmail.com>
2018-12-06 10:30:55 +0800

    move js to app/assets

--- app/assets/javascripts/baidu_ueditor.js
+++ app/assets/javascripts/baidu_ueditor.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ueditor/preinit
+//= require ueditor_custom/preinit
 //= require ueditor/ueditor.config
 //= require ueditor/ueditor.all
 
